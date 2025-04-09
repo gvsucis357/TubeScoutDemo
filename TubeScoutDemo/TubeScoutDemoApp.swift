e954282c80ec3f08a9553ee1ec76d27890ebe609
Jonathan Engelsma <jengelsma@gmail.com>
2025-04-09 15:05:19 -0400

    Initial Commit

---
+++ TubeScoutDemo/TubeScoutDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TubeScoutDemoApp.swift
+//  TubeScoutDemo
+//
+//  Created by Jonathan Engelsma on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct TubeScoutDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TubeScoutDemoTests/TubeScoutDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TubeScoutDemoTests.swift
+//  TubeScoutDemoTests
+//
+//  Created by Jonathan Engelsma on 4/9/25.
+//
+
+import Testing
+@testable import TubeScoutDemo
+
+struct TubeScoutDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
